a668bf213fb141ec1ed9a9ec26758424ca51db17
Christian Schulte <cs@schulte.it>
2025-12-16 06:59:38 +0000

    Register variables are zero indexed.
    git-svn-id: http://svn.jdtaus.org/svnroot/jdtaus/Abagnale/trunk@8996 d3f52c91-230f-0410-802c-e3ec080d4bae

--- abagnale.c
+++ abagnale.c
@@ -1042,16 +1042,16 @@ static void position_pricing(const struct worker_ctx *restrict const w_ctx,
     Numeric_add_to(p->price, r2, p->tp_price);
     Numeric_scale(p->tp_price, t->p_sc);
 
-    Numeric_sub_to(p->sl_price, p->price, r1);
-    if (Numeric_cmp(r1, p_inc) < 0) {
-      Numeric_add_to(p->sl_price, p_inc, r1);
-      Numeric_copy_to(r1, p->sl_price);
+    Numeric_sub_to(p->sl_price, p->price, r0);
+    if (Numeric_cmp(r0, p_inc) < 0) {
+      Numeric_add_to(p->sl_price, p_inc, r0);
+      Numeric_copy_to(r0, p->sl_price);
       Numeric_scale(p->sl_price, t->p_sc);
     }
-    Numeric_sub_to(p->tp_price, p->sl_price, r1);
-    if (Numeric_cmp(r1, p_inc) < 0) {
-      Numeric_add_to(p->tp_price, p_inc, r1);
-      Numeric_copy_to(r1, p->tp_price);
+    Numeric_sub_to(p->tp_price, p->sl_price, r0);
+    if (Numeric_cmp(r0, p_inc) < 0) {
+      Numeric_add_to(p->tp_price, p_inc, r0);
+      Numeric_copy_to(r0, p->tp_price);
       Numeric_scale(p->tp_price, t->p_sc);
     }
     break;
@@ -1061,17 +1061,17 @@ static void position_pricing(const struct worker_ctx *restrict const w_ctx,
     Numeric_sub_to(p->price, r2, p->tp_price);
     Numeric_scale(p->tp_price, t->p_sc);
 
-    Numeric_sub_to(p->price, p->sl_price, r1);
-    if (Numeric_cmp(r1, p_inc) < 0) {
-      Numeric_sub_to(p->sl_price, p_inc, r1);
-      Numeric_copy_to(r1, p->sl_price);
+    Numeric_sub_to(p->price, p->sl_price, r0);
+    if (Numeric_cmp(r0, p_inc) < 0) {
+      Numeric_sub_to(p->sl_price, p_inc, r0);
+      Numeric_copy_to(r0, p->sl_price);
       Numeric_scale(p->sl_price, t->p_sc);
     }
 
-    Numeric_sub_to(p->sl_price, p->tp_price, r1);
-    if (Numeric_cmp(r1, p_inc) < 0) {
-      Numeric_sub_to(p->tp_price, p_inc, r1);
-      Numeric_copy_to(r1, p->tp_price);
+    Numeric_sub_to(p->sl_price, p->tp_price, r0);
+    if (Numeric_cmp(r0, p_inc) < 0) {
+      Numeric_sub_to(p->tp_price, p_inc, r0);
+      Numeric_copy_to(r0, p->tp_price);
       Numeric_scale(p->tp_price, t->p_sc);
     }
     break;
