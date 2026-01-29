333160a83abb62e18b3aee132a4505efd0fad0a6
Christian Schulte <cs@schulte.it>
2026-01-29 12:46:25 +0000

    Avoid long/short terminology in verbose mode.
    git-svn-id: http://svn.jdtaus.org/svnroot/jdtaus/Abagnale/trunk@9115 d3f52c91-230f-0410-802c-e3ec080d4bae

--- abagnale.c
+++ abagnale.c
@@ -2110,10 +2110,10 @@ static void trade_bet(const struct worker_ctx *restrict const w_ctx,
         char *restrict const c = candle_string(
             &t->open_cd, String_chars(w_ctx->m->q_id), w_ctx->m->p_sc);
 
-        wout("%s: %s->%s: Cannot open %s long position: %s required: %s%s, %s "
-             "available: %s%s, candle: %s\n",
+        wout("%s: %s->%s: Cannot demand %s%s: %s required: %s%s, %s available: "
+             "%s%s, candle: %s\n",
              String_chars(w_ctx->e->nm), String_chars(w_ctx->m->q_id),
-             String_chars(w_ctx->m->b_id), String_chars(w_ctx->m->b_id),
+             String_chars(w_ctx->m->b_id), b, String_chars(w_ctx->m->b_id),
              String_chars(w_ctx->m->q_id), r, String_chars(w_ctx->m->q_id),
              String_chars(w_ctx->m->q_id), a, String_chars(w_ctx->m->q_id), c);
 
@@ -2168,11 +2168,10 @@ static void trade_bet(const struct worker_ctx *restrict const w_ctx,
         char *restrict const c = candle_string(
             &t->open_cd, String_chars(w_ctx->m->q_id), w_ctx->m->p_sc);
 
-        wout("%s: %s->%s: Cannot open %s short position: %s required: %s%s, %s "
-             "available: "
+        wout("%s: %s->%s: Cannot supply %s%s: %s required: %s%s, %s available: "
              "%s%s, %s required: %s%s, %s available: %s%s, candle: %s\n",
              String_chars(w_ctx->e->nm), String_chars(w_ctx->m->q_id),
-             String_chars(w_ctx->m->b_id), String_chars(w_ctx->m->b_id),
+             String_chars(w_ctx->m->b_id), b, String_chars(w_ctx->m->b_id),
              String_chars(w_ctx->m->q_id), qr, String_chars(w_ctx->m->q_id),
              String_chars(w_ctx->m->q_id), qa, String_chars(w_ctx->m->q_id),
              String_chars(w_ctx->m->b_id), b, String_chars(w_ctx->m->b_id),
