0f1aad6956f7ccab547bfe8415ad088a4c241cda
Christian Schulte <cs@schulte.it>
2026-07-22 05:09:33 +0200

    Mirror Subversion keywords

--- time.c
+++ time.c
@@ -1,5 +1,5 @@
 /* $SchulteIT: time.c 15189 2025-10-27 05:41:45Z schulte $ */
-/* $JDTAUS$ */
+/* $JDTAUS: time.c 9634 2026-07-22 02:43:37Z schulte $ */
 
 /*
  * Copyright (c) 2018 - 2026 Christian Schulte <cs@schulte.it>
