52439efe393a3ed4f6e2728e29d56882d9bd2b9a
Christian Schulte <cs@schulte.it>
2026-08-01 15:10:10 +0200

    Mirror Subversion keywords

--- exchange-coinbase.c
+++ exchange-coinbase.c
@@ -1,5 +1,5 @@
 /* $SchulteIT: exchange-coinbase.c 15282 2025-11-05 22:54:21Z schulte $ */
-/* $JDTAUS$ */
+/* $JDTAUS: exchange-coinbase.c 9654 2026-08-01 12:47:28Z schulte $ */
 
 /*
  * Copyright (c) 2018 - 2026 Christian Schulte <cs@schulte.it>
--- version.h
+++ version.h
@@ -17,6 +17,6 @@
 #ifndef VERSION_H
 #define VERSION_H
 
-#define ABAG_REVISION "$Abagnale$"
+#define ABAG_REVISION "$Abagnale: v0/9655 2026-08-01 13:10:06Z $"
 
 #endif
