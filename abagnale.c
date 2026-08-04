328121ffe91753197f0da9bd496427eb6dd07047
Christian Schulte <cs@schulte.it>
2026-08-04 15:59:44 +0000

    An error message has been updated to an informational message
    Following the changes introduced in revision 9663 a message written to
    the standard error stream has been updated to be written to the standard
    output stream instead. Prior to revision 9663 it was a clear user mistake
    and the message correctly got written to the standard error stream. As
    of revision 9663 the message needs to become informational because there
    is no way to distinguish between a mistake or intention. Especially in
    distributed setups the message can be confusing because there a process
    intentionally may not be configured to process a given market.
    
    
    
    
    git-svn-id: http://svn.jdtaus.org/svnroot/jdtaus/Abagnale/trunk@9667 d3f52c91-230f-0410-802c-e3ec080d4bae

--- abagnale.c
+++ abagnale.c
@@ -3339,7 +3339,7 @@ static int samples_process(void *restrict const arg) {
           mutex_unlock(&t->mtx);
         }
       } else {
-        werr("%s: %s: Trade: No matching market configuration - ignoring: %s\n",
+        wout("%s: %s: Trade: No matching market configuration - ignoring: %s\n",
              String_chars(w_ctx->e->nm), String_chars(w_ctx->m->nm),
              String_chars(t->id));
 
