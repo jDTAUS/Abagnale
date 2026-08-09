81e45b66e46138e9cd38cdba2ceae0fa56e079b6
Christian Schulte <cs@schulte.it>
2026-08-09 18:57:46 +0200

    Mirror Subversion keywords

--- exchange-coinbase.c
+++ exchange-coinbase.c
@@ -1,5 +1,5 @@
 /* $SchulteIT: exchange-coinbase.c 15282 2025-11-05 22:54:21Z schulte $ */
-/* $JDTAUS$ */
+/* $JDTAUS: exchange-coinbase.c 9664 2026-08-03 11:36:49Z schulte $ */
 
 /*
  * Copyright (c) 2018 - 2026 Christian Schulte <cs@schulte.it>
