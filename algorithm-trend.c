3b7cf49b1262ddbe0df51524a8e636e87efaec64
Christian Schulte <cs@schulte.it>
2026-08-16 10:34:20 +0200

    Mirror Subversion keywords

--- algorithm-trend.c
+++ algorithm-trend.c
@@ -1,5 +1,5 @@
 /* $SchulteIT: algorithm-trend.c 15268 2025-11-04 21:48:33Z schulte $ */
-/* $JDTAUS$ */
+/* $JDTAUS: algorithm-trend.c 9670 2026-08-05 05:19:23Z schulte $ */
 
 /*
  * Copyright (c) 2018 - 2026 Christian Schulte <cs@schulte.it>
--- version.h
+++ version.h
@@ -17,6 +17,6 @@
 #ifndef VERSION_H
 #define VERSION_H
 
-#define ABAG_REVISION "$Abagnale$"
+#define ABAG_REVISION "$Abagnale: v0/9681 2026-08-16 08:34:17Z $"
 
 #endif
