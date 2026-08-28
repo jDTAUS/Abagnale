cbdc88ab54a397b175de59f8bfbb11e4fc561dad
Christian Schulte <cs@schulte.it>
2026-08-28 08:47:40 +0200

    Mirror Subversion keywords

--- abagnale.c
+++ abagnale.c
@@ -1,5 +1,5 @@
 /* $SchulteIT: abagnale.c 15281 2025-11-05 06:02:51Z schulte $ */
-/* $JDTAUS$ */
+/* $JDTAUS: abagnale.c 9696 2026-08-25 03:59:29Z schulte $ */
 
 /*
  * Copyright (c) 2018 - 2026 Christian Schulte <cs@schulte.it>
--- version.h
+++ version.h
@@ -17,6 +17,6 @@
 #ifndef VERSION_H
 #define VERSION_H
 
-#define ABAG_REVISION "$Abagnale$"
+#define ABAG_REVISION "$Abagnale: v0/9700 2026-08-28 06:47:37Z $"
 
 #endif
