9ed2fb0e69f62a7d9df7569e7b5c6a1f0fd0791a
Christian Schulte <cs@schulte.it>
2026-08-29 11:54:02 +0000

    Message improvements
    The abagnale application abagnale.c has been updated to use unscaled
    arbitrary precision fee and volatility values in messages and to inform
    about effective market configurations.
    
    Function Numeric_to_char_unscaled has been added to the math API math.h
    and to the math-postgresql.c implementation.
    
    
    git-svn-id: http://svn.jdtaus.org/svnroot/jdtaus/Abagnale/trunk@9707 d3f52c91-230f-0410-802c-e3ec080d4bae

--- abagnale.c
+++ abagnale.c
@@ -299,9 +299,9 @@ const struct MarketConfig *marketconfig(struct String *restrict const e_nm,
       char *restrict const w_ns = nanos_string(m_cnf->wnanos);
       char *restrict const ret = Numeric_to_char_unscaled(m_cnf->r_amount);
 
-      wout("%s: Configuration: %s using %s return %s%s window %s\n",
-           String_chars(e_nm), String_chars(m_nm), String_chars(m_cnf->a_nm),
-           ret, String_chars(m_cnf->r_id), w_ns);
+      wout("%s: %s using %s return %s%s window %s\n", String_chars(e_nm),
+           String_chars(m_nm), String_chars(m_cnf->a_nm), ret,
+           String_chars(m_cnf->r_id), w_ns);
 
       Numeric_char_free(ret);
       heap_free(w_ns);
