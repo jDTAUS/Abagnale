2d8fa55843b4f67e131af2d03326fff323e3ea9e
Christian Schulte <cs@schulte.it>
2026-09-13 23:12:11 +0200

    Mirror Subversion keywords

--- map.h
+++ map.h
@@ -1,5 +1,5 @@
 /* $SchulteIT: map.h 15189 2025-10-27 05:41:45Z schulte $ */
-/* $JDTAUS$ */
+/* $JDTAUS: map.h 9643 2026-07-29 08:15:42Z schulte $ */
 
 /*
  * Copyright (c) 2018 - 2026 Christian Schulte <cs@schulte.it>
