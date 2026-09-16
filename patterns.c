ddcc7d8da30f8beabdfcefc86d3050cc28f48fe9
Christian Schulte <cs@schulte.it>
2026-09-16 10:17:27 +0200

    Mirror Subversion keywords

--- patterns.c
+++ patterns.c
@@ -33,7 +33,7 @@
 
 /*
  * $SchulteIT: patterns.c 15221 2025-10-28 07:11:14Z schulte $
- * $JDTAUS$
+ * $JDTAUS: patterns.c 9714 2026-09-07 11:09:55Z schulte $
  * Origin: OpenBSD 7.8 httpd/patterns.c
  * Modifications: Use standard C intmax_t instead of POSIX off_t
  *                Use struct String instead of char
--- version.h
+++ version.h
@@ -17,6 +17,6 @@
 #ifndef VERSION_H
 #define VERSION_H
 
-#define ABAG_REVISION "$Abagnale$"
+#define ABAG_REVISION "$Abagnale: v0/9735 2026-09-16 08:17:24Z $"
 
 #endif
