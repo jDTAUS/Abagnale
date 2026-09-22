8e1811238f0a3c0720b1bbc67c340ff8d490ccf9
Christian Schulte <cs@schulte.it>
2026-09-22 11:55:44 +0200

    Mirror Subversion keywords

--- abagnale.c
+++ abagnale.c
@@ -1,5 +1,5 @@
 /* $SchulteIT: abagnale.c 15281 2025-11-05 06:02:51Z schulte $ */
-/* $JDTAUS$ */
+/* $JDTAUS: abagnale.c 9740 2026-09-22 09:55:41Z schulte $ */
 
 /*
  * Copyright (c) 2018 - 2026 Christian Schulte <cs@schulte.it>
