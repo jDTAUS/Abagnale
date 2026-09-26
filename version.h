67546932f68f86530109ab600b4eb8ca7d9a9396
Christian Schulte <cs@schulte.it>
2026-09-26 14:11:53 +0200

    Mirror Subversion keywords

--- version.h
+++ version.h
@@ -1,7 +1,7 @@
 #ifndef VERSION_H
 #define VERSION_H
 
-#define ABAG_REVISION "$Abagnale$"
+#define ABAG_REVISION "$Abagnale: v0/9767 2026-09-26 12:11:39Z $"
 
 /*
  * Copyright (c) 2018 - 2026 Christian Schulte <cs@schulte.it>
