9c61b5b1d57471115af8d966d613480bcbabe556
Christian Schulte <cs@schulte.it>
2026-09-26 09:57:06 +0200

    Mirror Subversion keywords

--- queue.h
+++ queue.h
@@ -1,5 +1,5 @@
 /* $SchulteIT: queue.h 15189 2025-10-27 05:41:45Z schulte $ */
-/* $JDTAUS$ */
+/* $JDTAUS: queue.h 9760 2026-09-26 07:44:41Z schulte $ */
 
 /*
  * Copyright (c) 2018 - 2026 Christian Schulte <cs@schulte.it>
