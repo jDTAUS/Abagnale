00f1aca68852c095a0fb7f8ae641b9556b259583
Christian Schulte <cs@schulte.it>
2026-09-26 09:49:38 +0200

    Mirror Subversion keywords

--- map.h
+++ map.h
@@ -1,5 +1,5 @@
 /* $SchulteIT: map.h 15189 2025-10-27 05:41:45Z schulte $ */
-/* $JDTAUS$ */
+/* $JDTAUS: map.h 9761 2026-09-26 07:49:34Z schulte $ */
 
 /*
  * Copyright (c) 2018 - 2026 Christian Schulte <cs@schulte.it>
--- version.h
+++ version.h
@@ -17,6 +17,6 @@
 #ifndef VERSION_H
 #define VERSION_H
 
-#define ABAG_REVISION "$Abagnale$"
+#define ABAG_REVISION "$Abagnale: v0/9761 2026-09-26 07:49:34Z $"
 
 #endif
